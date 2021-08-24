477eb299df81f3ba61139aaa979deb9c2b26db69
SRC0747 <RjSrc@0747>
2021-08-25 02:17:02 +0530

    Insertion Sort to sort the Words in String array.

---
+++ src/com/insertionsortstringarray/SortStringArrayUsingInsertionSort.java
@@ -0,0 +1,26 @@
+package com.insertionsortstringarray;
+
+public class SortStringArrayUsingInsertionSort {
+    public static void main(String[] args){
+        String[] arr ={"Kangaroo","Crocodile","Tiger","Dog","Monkey","Elephant","Lion"};
+        int count = 0;
+        String sortedArray[] = sort_sub(arr, arr.length);
+        for(int i=0;i<sortedArray.length;i++){
+            System.out.println(sortedArray[i]);
+        }
+    }
+
+    public static String[] sort_sub(String array[], int f){
+        String temp="";
+        for(int i=0;i<f;i++){
+            for(int j=i+1;j<f;j++){
+                if(array[i].compareToIgnoreCase(array[j])>0){
+                    temp = array[i];
+                    array[i]=array[j];
+                    array[j]=temp;
+                }
+            }
+        }
+        return array;
+    }
+}
